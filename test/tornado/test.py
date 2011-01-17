412becdcda73d631db4108344179b2722d089a7c
Thomas Rampelberg <thomas@saunter.org>
2011-01-16 23:33:45 -0800

    Really lame unit test for the tornado handler

--- test/tornado/test.py
+++ test/tornado/test.py
@@ -10,17 +10,22 @@ __author__ = 'Thomas Rampelberg'
 __author_email__ = 'thomas@saunter.org'
 
 import logging
+import re
 from tornado.options import define, options
 import tornado.testing
+import tornado.web
+import urllib
 import unittest
 
-class TestHandler(janky_post.Handler):
+import janky_post.tornado_handler
+
+class TestHandler(janky_post.tornado_handler.Handler):
     def get(self):
         self.write({ 'foo': 'bar' })
 
     post = get
 
-class AsyncHandler(janky_post.Handler):
+class AsyncHandler(janky_post.tornado_handler.Handler):
     
     @tornado.web.asynchronous
     def get(self):
@@ -32,7 +37,25 @@ class AsyncHandler(janky_post.Handler):
     def _woo(self):
         self.finish()
 
-class BaseTest(tornado.testing.AsyncHTTPTestCase,
-                 tornado.testing.LogTrapTestCase):
-    pass
+class HandlerTest(tornado.testing.AsyncHTTPTestCase,
+                  tornado.testing.LogTrapTestCase):
     
+    def get_app(self):
+        return tornado.web.Application([("/", TestHandler),
+                                        ("/a", AsyncHandler)])
+    
+
+    def test_get(self):
+        resp = self.fetch(
+            '/?' + urllib.urlencode({ 
+                        '_origin': 'http://example.com:8080/foo/bar'}))
+        
+        assert re.search('window.name', resp.body)
+        assert re.search('location.href = "http://example.com:8080/janky";',
+                         resp.body)
+
+def all():
+    return unittest.defaultTestLoader.loadTestsFromNames(['test'])
+
+if __name__ == '__main__':
+    tornado.testing.main()
