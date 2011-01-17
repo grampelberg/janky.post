95075c11d0062abf03b0cc18537dce120c73cc29
Thomas Rampelberg <thomas@saunter.org>
2011-01-17 15:13:56 -0800

    Need to add the demo script in

---
+++ test/demo.js
@@ -0,0 +1,29 @@
+/**
+ * @author: Thomas Rampelberg <thomas@saunter.org>
+ *
+ * Copyright(c) 2011 Thomas Rampelberg.
+ */
+
+$(function() {
+  $("form").submit(function() {
+    try {
+    janky({
+      url: 'http://localhost:5000/api',
+      method: 'post',
+      data: {
+        one: $("input[name=one]").val(),
+        two: $("input[name=two]").val(),
+        time: new Date().getTime()
+      },
+      success: function(resp) {
+        var elem = $("<pre><code></code></pre>");
+        var str = JSON.stringify(resp).split(',').join(',\r\n    ').split(
+          '{').join('{\r\n    ').split('}').join('\r\n}');
+        elem.find("code").html(str);
+        $("h2:contains(Output)").after(elem);
+      }
+    });
+    } catch(err) { console.log(err) }
+    return false;
+  });
+});
