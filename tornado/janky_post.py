fcb796008b2b4efcb2009c95d0c8065af4400886
Thomas Rampelberg <thomas@saunter.org>
2011-01-16 22:08:50 -0800

    And, docs for tornado put into the file

--- tornado/janky_post.py
+++ tornado/janky_post.py
@@ -2,8 +2,26 @@
 # Copyright (c) 2011 Thomas Rampelberg
 #
 
-"""
-Make doing janky cross domain communication easy.
+"""Making cross-domain requests easy on the server (for tornado).
+
+Here's a canonical use of janky.post in Tornado:
+
+    import janky_post
+    import tornado.ioloop
+    import tornado.web
+
+    class MainHandler(janky_post.Handler):
+        def get(self):
+            self.write({ "status": "ok", data: "foobar" })
+
+    if __name__ == "__main__":
+        application = tornado.web.Application([
+            (r"/", MainHandler),
+        ])
+        application.listen(8888)
+        tornado.ioloop.IOLoop.instance().start()
+
+See the readme at https://github.com/pyronicide/janky.post for more details.
 """
 
 __author__ = 'Thomas Rampelberg'
@@ -25,6 +43,7 @@ tmpl = """
 template = tornado.template.Template(tmpl)
 
 class Handler(tornado.web.RequestHandler):
+    """Subclass this class just as you would tornado.web.RequestHandler."""
 
     def finish(self, *args, **kwargs):
         self.set_header('Content-Type', 'text/html; charset=UTF-8')
