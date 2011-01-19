560fac7c125712b85bf033953a001bfcff6f0e47
Thomas Rampelberg <thomas@saunter.org>
2011-01-18 22:29:42 -0800

    And, a little more

--- python/janky_post/gae.py
+++ python/janky_post/gae.py
@@ -54,7 +54,7 @@ class JankyMiddleware(object):
                         'resp': json.dumps(body), 
                         'origin': json.dumps(urlparse.urljoin(
                                 webapp.Request(environ).get('_origin'), 
-                                '/janky.html'))
+                                '/janky'))
                         }
                 write(body)
             return my_write
